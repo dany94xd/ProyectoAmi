57b6030bc08f11f29ce09db805ba58030ce10f30
CesarxArteaga <ctmces94@gmail.com>
2019-07-24 22:42:37 -0500

    remove all

--- server/build/controllers/indexControllers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class IndexControllers {
-    index(req, res) {
-        res.send('Hello');
-    }
-}
-exports.indexControllers = new IndexControllers();
