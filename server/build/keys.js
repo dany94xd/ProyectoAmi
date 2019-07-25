0e5b530bf94d62bceaf978fabdb61960f4c9c7c3
CesarxArteaga <ctmces94@gmail.com>
2019-07-24 23:39:37 -0500

    autenticacion mysql

--- server/build/keys.js
+++ server/build/keys.js
@@ -5,6 +5,7 @@ exports.default = {
         host: '127.0.0.1',
         user: 'root',
         password: 'T4rj3t4.V3rd3',
-        database: 'tarjetaverde'
+        database: 'tarjetaverde',
+        insecureAuth : true
     }
 };
