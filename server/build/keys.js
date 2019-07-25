124d173c1d9ef44f63a0cbc9baca935e4923b875
CesarxArteaga <ctmces94@gmail.com>
2019-07-24 23:26:40 -0500

    coreccion puerto

--- server/build/keys.js
+++ server/build/keys.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'lo127.0.0.1',
+        host: '127.0.0.1',
         user: 'root',
         password: 'T4rj3t4.V3rd3',
         database: 'tarjetaverde'
