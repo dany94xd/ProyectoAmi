07ef7831c573a3e2378f4ea5722cb6b673da75ea
CesarxArteaga <ctmces94@gmail.com>
2019-07-24 23:33:57 -0500

    puerto cambio

--- server/build/index.js
+++ server/build/index.js
@@ -15,7 +15,7 @@ class Server {
         this.routes();
     }
     config() {
-        this.app.set('port', process.env.port || 3003);
+        this.app.set('port', process.env.port || 5100);
         this.app.use(morgan_1.default('dev'));
         this.app.use(cors_1.default());
         this.app.use(express_1.default.json());
