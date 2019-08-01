b159075a6c8bd0c7bd4bd19e64e441a755710f97
hugoclavijo <hugoused@hotmail.com>
2019-07-31 22:52:55 -0500

    busqueda tablas

--- controllers/mobileapp.controller.js
+++ controllers/mobileapp.controller.js
@@ -31,7 +31,7 @@ mobileAppCtrl.getMatriculaData = async (req, res, next) => {
     
     const usuarioData = await Usuario.find({ 'matricula': id}).select({ idInstitucion:1, matricula:1, NroBotellas: 1, saldoTotal: 1, saldoActual: 1 });
  
- const personaId = await Usuario.find({ 'matricula': id}).select({ idPersona:1});
+ const { personaId } = await Usuario.find({ 'matricula': id}).select({ idPersona:1});
 
  //const personaData = await Persona.find({ 'matricula': id }).select({ nombre: 1, apellido: 1});
    const personaData = await Persona.find({ '_id': personaId }).select({ nombre: 1, apellido: 1}); 
