63e969c304efcfd779560e8e41db642a84604428
hugoclavijo <hugoused@hotmail.com>
2019-07-31 22:14:48 -0500

    Busqueda de matricula por id por persona

--- controllers/mobileapp.controller.js
+++ controllers/mobileapp.controller.js
@@ -31,10 +31,10 @@ mobileAppCtrl.getMatriculaData = async (req, res, next) => {
     
     const usuarioData = await Usuario.find({ 'matricula': id}).select({ idInstitucion:1, matricula:1, NroBotellas: 1, saldoTotal: 1, saldoActual: 1 });
  
- const userId = await Usuario.find({ 'matricula': id}).select({ id:1});
+ const personaId = await Usuario.find({ 'matricula': id}).select({ idPersona:1});
 
  //const personaData = await Persona.find({ 'matricula': id }).select({ nombre: 1, apellido: 1});
-   const personaData = await Persona.find({ 'id': userId }).select({ nombre: 1, apellido: 1}); 
+   const personaData = await Persona.find({ '_id': personaId }).select({ nombre: 1, apellido: 1}); 
     
     const dataFinal= [await usuarioData, await personaData];
     
