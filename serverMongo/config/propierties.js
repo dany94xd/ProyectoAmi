10cff24444453a9bac13aa236c3e833f16e1b929
CesarxArteaga <ctmces94@gmail.com>
2019-08-01 00:21:42 -0500

    acceso remoto

--- serverMongo/config/propierties.js
+++ serverMongo/config/propierties.js
@@ -2,6 +2,6 @@ module.exports={
 
     PORT: process.env.PORT || 4000,
     //DB: 'mongodb://localhost:27017/persona'
-    DB:'mongodb://interfaz:cesarwey123@162.212.130.145:27017/persona'
+    DB: 'mongodb://interfacez:cesar1234@162.212.130.145:27017/persona'
 
 }
