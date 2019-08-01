1aad31b8db2ad239cb9258982b803c9ee1255ce7
hugoclavijo <hugoused@hotmail.com>
2019-07-31 23:16:18 -0500

    mobilapp controller

--- controllers/mobileapp.controller.js
+++ controllers/mobileapp.controller.js
@@ -34,11 +34,11 @@ mobileAppCtrl.getMatriculaData = async (req, res, next) => {
  const { personaId } = await Usuario.find({ 'matricula': id}).select({ idPersona:1});
 
  //const personaData = await Persona.find({ 'matricula': id }).select({ nombre: 1, apellido: 1});
-   const personaData = await Persona.find({ '_id': personaId }).select({ nombre: 1, apellido: 1}); 
+   const personaData = await Persona.find({ '_id': personaId.idPersona }).select({ nombre: 1, apellido: 1}); 
     
     const dataFinal= [await usuarioData, await personaData];
     
-    console.log(dataFinal);
+    //console.log(dataFinal);
     res.json(dataFinal);
 };
 
