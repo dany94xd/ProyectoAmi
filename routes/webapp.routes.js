5de739302422514ac95eea2d78cf96c023de4875
Hector-Luis <hluischimbo@gmail.com>
2019-08-13 08:16:56 -0500

    api web reciclador

--- routes/webapp.routes.js
+++ routes/webapp.routes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const webapp = require('../controllers/webapp.controller');
 
 router.get('/data/:id', webapp.getMatriculaData);
+router.get('/userdata/:id', webapp.getUserData);
+router.get('/userupdatesaldo/:id', webapp.userUpdateSaldo);
 
 module.exports = router;
