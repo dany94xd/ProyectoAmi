ffd731aa7ab5291abd4b17421c9c51427ed75bbc
dany94xd <danimaxi60@hotmail.com>
2019-09-03 22:28:01 -0500

    dist interface v1

--- procit.js
+++ procit.js
@@ -1,27 +1,71 @@
+'use strict'
+
+//const authRoutes = require('./routes/auth.routes')
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const router = express.Router(); // rutas primera version
+const propierties = require('../../config/propierties'); // propiedades de la base
+const DB =require('../../config/db') //url de la base 
 
+//const { mongoose } = require('./db');
 var path = require('path')
 
+//instancia base
+DB();
+
+
+//body parse
+// const bodyParse=require('body-parser')
+// const bodyParseJson=bodyParse.json()
+// const bodyParserURLEncoded = bodyParse.urlencoded({extended:true})
+
+// //usando el body parse
+// app.use(bodyParseJson)
+// app.use(bodyParserURLEncoded)
+//app.use('/api',router)
+//authRoutes(router)
+//router.get('/',(req,res)=>{
+ //   res.send("hello")
+//}) //rama home prueba pero ya no sirve por el dist
+
+//app.use(router)
+
+
+
 // Settings
-app.set('port', process.env.PORT || 5110);
-
-
+app.set('port', process.env.PORT || 3004);
 
 // Middlewares
 app.use('/',express.static('client',{redirect:false}));
 app.use(cors({origin: 'http://162.212.130.145'}));
+//app.use(cors({origin: 'http://localhost:4200'}));
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
-  app.use(express.json());
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(express.json());
 
-  //starting the server
+// Routes
+app.use('/api/persona', require('../../routes/employee.routes'));
+
+
+
+
+//starting the server
+
   app.get('*',function(req,res,next){
-    res.sendFile(path.resolve('client/index.html'))
-  });
+       res.sendFile(path.resolve('client/index.html'))
+     });
+ 
+     
+//antigua forma de ver puerto por donde corre el node     
+// app.listen(app.get('port'), () => {
+//     console.log(`server on port ${app.get('port')}`);
+// });
+
+// nueva forma de ver el puerto
+app.listen(propierties.PORT,()=>console.log(`server runing on port ${propierties.PORT}`))
--- serverMongo/config/propierties.js
+++ serverMongo/config/propierties.js
@@ -1,6 +1,6 @@
 module.exports={
 
-    PORT: process.env.PORT || 4000,
+    PORT: process.env.PORT || 3004,
     //DB: 'mongodb://localhost:27017/persona'
     DB: 'mongodb://interfacez:cesar1234@162.212.130.145:27017/persona'
 
