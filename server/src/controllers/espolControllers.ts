af117aa5da7ce90a5cb25cfd8f095dca78cb8e57
CesarxArteaga <ctmces94@gmail.com>
2019-09-05 11:06:07 -0500

    update and getone methods

--- server/src/controllers/espolControllers.ts
+++ server/src/controllers/espolControllers.ts
@@ -10,6 +10,25 @@ class EspolControllers{
     public create (req:Request, res:Response){
         res.json({text:'creating a'});
     }
+
+    public async getOne(req: Request, res: Response): Promise<any> {
+        const { id } = req.params;
+        const games = await pool.query('select usuario.id_persona, usuario.identificacion, persona.cedula, persona.nombres, persona.apellidos from usuario inner join persona on usuario.id_persona=persona.id_persona WHERE persona.cedula = ?', [id]);
+        console.log(games.length);
+        if (games.length > 0) {
+            return res.json(games[0]);
+        }
+        res.status(404).json({ text: "La persona no existe" });
+    } 
+
+    public async update (req:Request, res:Response): Promise<void>{
+        const { id } = req.params;
+        const oldGame = req.body;
+        await pool.query('UPDATE persona set ? WHERE id_persona = ?', [req.body, id]);
+        res.json({ message: "La persona fue actualizado" });
+    }
+
+    
 }
 const espolControllers= new EspolControllers();
 export default espolControllers;
--- server/src/controllers/indexControllers.ts
+++ server/src/controllers/indexControllers.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from 'express';
+import pool from '../database'
 
 class IndexControllers{
 
     index (req:Request, res:Response){
         res.send('Hello');
     } 
+
+    public async updateUser (req:Request, res:Response): Promise<void>{
+        const { id } = req.params;
+        const oldGame = req.body;
+        await pool.query('UPDATE usuario set ? WHERE id_persona = ?', [req.body, id]);
+        res.json({ message: "El usuario fue actualizado" });
+    }
 
 }
 export const indexControllers= new IndexControllers();
